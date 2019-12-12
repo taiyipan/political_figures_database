79e06014db63b5c10dfc389f360399fc8ab262d4
Taiyi Pan <taiyipan@gmail.com>
2019-12-12 00:34:07 -0800

    Major update: optimized query by replacing loop with single SQL statement; added a question column to member page in order to uniquely identify a vote's identity in addition to bill_short_title column; updated index.html; deleted unnecessary files

--- search.php
+++ search.php
@@ -8,10 +8,10 @@
   //branch into 4 paths
   switch ($area) {
     case 'house_members':
-      $tokens = explode(' ', $input, 2);
+      $tokens = explode(' ', $input, 2); //split string on first space
       $first_name = $tokens[0];
       $last_name = $tokens[1];
-      //edge cases
+      //edge case handling 
       if ($input == 'C.A. Dutch Ruppersberger') {
         $first_name = 'C.A. Dutch';
         $last_name = 'Ruppersberger';
@@ -20,7 +20,7 @@
       break;
 
     case 'senate_members':
-      $tokens = explode(' ', $input, 2);
+      $tokens = explode(' ', $input, 2); //split string on first space
       $first_name = $tokens[0];
       $last_name = $tokens[1];
       include_once "pages/senate_member.php";
